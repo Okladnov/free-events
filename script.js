bf6bfb7cc7812c5a979d0977712e6fd18eb5f780
Okladnov <okladnov.poco@yandex.ru>
2026-02-11 15:17:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -194,6 +194,7 @@ async function loadEvents(isNewSearch = false) {
         
         cardClone.querySelector('.card-date').textContent = `Опубликовано ${new Date(event.created_at).toLocaleDateString()}`;
         cardClone.querySelector('.card-title').textContent = event.title;
+        const cleanDescription = (event.description || '').replace(/<[^>]*>?/gm, '');
         cardClone.querySelector('.card-description').textContent = `${(event.description || '').substring(0, 100)}...`;
         
         const image = cardClone.querySelector('.card-image');
